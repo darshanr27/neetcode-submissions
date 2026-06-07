37fa18684753a66a5dd601c8f1ef3f5d97b245f5
Darshan R <41225547+darshanr27@users.noreply.github.com>
2026-06-07 11:53:52 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,36 @@
+class Solution:
+    def mergeSort(self, arr):
+        if len(arr) <= 1:
+            return arr
+        
+        mid = len(arr)//2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        left_sorted = self.mergeSort(left_half)
+        right_sorted = self.mergeSort(right_half)
+
+        return self.merge(left_sorted, right_sorted)
+
+    def merge(self, left, right):
+        result = []
+        i = j = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                result.append(left[i])
+                i += 1
+            else:
+                result.append(right[j])
+                j += 1
+            
+        result.extend(left[i:])
+        result.extend(right[j:])
+        return result
+
+    def isAnagram(self, s: str, t: str) -> bool:
+        sortedS = "".join(self.mergeSort(list(s)))
+        sortedT = "".join(self.mergeSort(list(t)))
+
+        return sortedS == sortedT
+
